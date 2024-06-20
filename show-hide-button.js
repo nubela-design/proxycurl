9b1ea8f0eb1885de6afa0a9f68c0157b9f1d98b2
ajiwandi <aji@nubela.co>
2024-06-20 16:14:36 +0700

    update

---
+++ show-hide-button.js
@@ -0,0 +1,31 @@
+function toggleHeight1(button) {
+    const preContainer = button.previousElementSibling;
+    if (preContainer.style.maxHeight === 'none') {
+      preContainer.style.maxHeight = '176px';
+      button.textContent = 'Show More';
+    } else {
+      preContainer.style.maxHeight = 'none';
+      button.textContent = 'Show Less';
+    }
+  }
+
+  function toggleHeight2(button) {
+    const preContainer = button.previousElementSibling;
+    if (preContainer.style.maxHeight === 'none') {
+      preContainer.style.maxHeight = '176px';
+      button.textContent = 'Show More';
+    } else {
+      preContainer.style.maxHeight = 'none';
+      button.textContent = 'Show Less';
+    }
+  }  
+  function toggleHeight3(button) {
+    const preContainer = button.previousElementSibling;
+    if (preContainer.style.maxHeight === 'none') {
+      preContainer.style.maxHeight = '176px';
+      button.textContent = 'Show More';
+    } else {
+      preContainer.style.maxHeight = 'none';
+      button.textContent = 'Show Less';
+    }
+  }  
